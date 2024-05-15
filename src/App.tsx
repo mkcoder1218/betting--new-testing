cb64ea2b205c1e3b980f135b2fa2d38c4c369c79
Amanuel <amanuelshumi14@gmail.com>
2024-05-15 07:31:49 +0300

    component breakdown from App file

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import TicketSelector from './components/TicketSelector';
 import { useState } from 'react';
 import CashierOptions from './components/CashierOptions';
 import RedeemTicket from './components/RedeemTicket';
+import BetSlip from './components/BetSlip';
 
 function App() {
 
@@ -49,59 +50,7 @@ function App() {
             <TicketSlipHolder />
           </div>
         </div>
-        <div className='right basis-1/2 flex items-center flex-col'>
-          <div className='text-l text-orange-500 font-bold flex items-center justify-center text-center'>
-            Betslip
-          </div>
-          <div className='right-slip-content w-full flex flex-col items-center p-4'>
-            <div className="slip-right-head flex items-center justify-center bg-orange-500 rounded-md p-1">
-              <div className="left bg-orange-500 p-1 pr-2 pl-2 text-sm text-white rounded-md">
-                SINGLE
-              </div>
-              <div className="left bg-black p-1 pr-2 pl-2 text-sm text-white rounded-md">
-                MULTIPLES
-              </div>
-            </div>
-
-            <div className="selected-nums-con w-3/4 bg-gray-500 rounded-md p-2 mt-4 text-white">
-              <p className='text-xs mb-2 flex items-center'><span className='rounded-xl h-5 w-5 flex items-center justify-center border-2 mr-2'>8</span> Win</p>
-              <p className='text-xs'>2,3,4,5 <span className='bg-amber-600 p-1 text-white rounded-lg text-xs'>400</span></p>
-              <p className='text-xs mt-1'>2024/10/10 10:22:20 ID|5463</p>
-              <div className="inc-dec mt-2 flex bg-white p-1 items-center justify-between flex-shrink-0">
-                <div className='text-white hover:bg-gray-500 cursor-pointer transition-all h-6 w-6 justify-center inc bg-black rounded-md flex items-center p-1'>
-                  +
-                </div>
-                <div className='num text-black'>
-                  20
-                </div>
-                <div className='text-white hover:bg-gray-500 cursor-pointer transition-all h-6 w-6 justify-center dec bg-black rounded-md flex items-center p-1'>
-                  -
-                </div>
-              </div>
-              <p className='text-white text-xs text-center mt-1'>TO WIN Br. 4000</p>
-            </div>
-            <div className='btn-container-bet mt-2 flex gap-2 justify-stretch w-3/4 items-center'>
-              <button className='bg-green-600 hover:opacity-75 transition-all flex-grow p-3 rounded-md text-white'>10 <span className='ml-3'>$</span> </button>
-              <button className='bg-pink-600 hover:opacity-75 transition-all flex-grow p-3 rounded-md text-white'>20 <span className='ml-3'>$</span> </button>
-              <button className='bg-blue-600 hover:opacity-75 transition-all flex-grow p-3 rounded-md text-white'>50 <span className='ml-3'>$</span> </button>
-              <button className='bg-blue-400 hover:opacity-75 transition-all flex-grow p-3 rounded-md text-white'>100 <span className='ml-3'>$</span> </button>
-            </div>
-            <div className="amounts mt-2 w-3/4 text-black">
-              <div className='text-lg mt-1 flex justify-between items-center'>
-                <p>TOTAL STAKE</p>
-                <p>100.00 BR</p>
-              </div>
-              <div className='text-lg mt-1 flex justify-between items-center'>
-                <p>TOTAL "TO WIN"</p>
-                <p>100.00 BR</p>
-              </div>
-            </div>
-            <div className='confirm-cancel w-3/4 gap-1 text-white mt-2 flex justify-between items-center'>
-              <button className='p-3 flex-grow hover:opacity-75 transition-opacity bg-red-500'>CANCEL</button>
-              <button className='p-3 flex-grow hover:opacity-75 transition-opacity basis-3/4 bg-green-500'>PLACE BET</button>
-            </div>
-          </div>
-        </div>
+        <BetSlip />
       </div>
     </div>
   )
