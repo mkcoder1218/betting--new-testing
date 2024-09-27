7edf950007d7e57057fad5521f0873defacf3e25
kayo <kayomelese4@gmail.com>
2024-09-27 16:31:47 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -227,7 +227,7 @@ function App() {
   return (
     <div className="bg-white fixed w-full h-full custom-scrollbar overflow-y-auto">
       <PrinterDialog
-        open={printerDialog}
+        open={false}
         handleClose={handlePrintDialogClose}
         logout={logout}
       />
