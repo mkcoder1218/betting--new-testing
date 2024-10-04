052d5eca10b51910c258f27cba0bd3003347eaad
kayo <kayomelese4@gmail.com>
2024-10-05 01:04:52 +0300

    update

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,5 +2,5 @@ export const LOCAL_USER = "user_session";
 const url: string = window.location.href;
 const arr: string[] = url.split("/");
 const newurl: string[] = arr[2].split(":");
-export const SERVER_URI = `http://3.142.191.202:9000//api/v1/`;
+export const SERVER_URI = `https://retail2.playbetman2.com/api/api/v1/`;
 export const defaultStake = 10.0;
