45bed62e65b7a5ed521365a956909754490145d8
kayo <kayomelese4@gmail.com>
2024-10-04 23:47:28 +0300

    update

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,5 +2,5 @@ export const LOCAL_USER = "user_session";
 const url: string = window.location.href;
 const arr: string[] = url.split("/");
 const newurl: string[] = arr[2].split(":");
-export const SERVER_URI = `http://18.188.141.106/api/v1/`;
+export const SERVER_URI = `http://https://3.142.191.202:9000//api/v1/`;
 export const defaultStake = 10.0;
